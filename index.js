bf1e781a38e47d1f2ddb8f40bc4de42db78d4d99
Kader B <kder000@gmail.com>
2023-04-27 12:24:36 +0200

    heracles1 done

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require('./src/Fighter')
+
+const heracles = new Fighter('🧔 Heracles', 20, 6);
+const nemeanLion = new Fighter('🦁 Nemean Lion', 11, 13);
+
+
+let round = 1;
+
+do{
+    //instructions
+    console.log(`Round #${round}`);
+    heracles.fight(nemeanLion);
+    if(nemeanLion.isAlive()) {
+        nemeanLion.fight(heracles);
+    }
+    round++;
+
+} while(heracles.isAlive() && nemeanLion.isAlive())
+
+
+const winner = heracles.isAlive() ? heracles : nemeanLion;
+const loser = heracles.isAlive() ? nemeanLion : heracles;
+
+console.log(`${loser.name} is dead`);
+console.log(`${winner.name} wins ${winner.life} HP`);
+
+
+//heracles.fight(nemeanLion);
+//nemeanLion.fight(heracles); 
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,41 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor (name, strength, dexterity) {
+    this.name = name;
+    this.strength =strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+    console.log(`${this.name} has been created. It has ${this.life} HP`);
+    }
+
+
+
+fight(enemy) {
+    let damage = this.generateRandomInteger(this.strength);
+    let mitigatedDamage = damage - enemy.dexterity;
+    if (mitigatedDamage > 0) {
+        enemy.life = enemy.life - mitigatedDamage;
+        if(enemy.life < 0) {
+            enemy.life = 0;
+            console.log(`${enemy.name} took ${mitigatedDamage} damage. It has ${enemy.life} HP`);
+        } else {
+            console.log(`${this.name}'s attacks failed!`);
+        }
+    }
+
+}
+
+generateRandomInteger(max) {
+   return Math.floor(Math.random() * max + 1);
+}
+
+isAlive() {
+    return this.life > 0;
+}
+
+}
+
+module.exports = Fighter;
